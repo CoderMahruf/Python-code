0e195567e07c00e45c1fadbdbd5c91d02ce465d7
mahruf60 <mahruf9060@gmail.com>
2024-10-03 22:27:20 +0600

    update

---
+++ short_hand_if-else.py
@@ -0,0 +1,13 @@
+a = 2
+b = 330
+print("A") if a > b else print("B")
+
+a= 30300
+b=303
+#                 #                       #                #
+print("A") if a > b else print("=") if a==b else print("B")
+
+print(9) if a>b else ""
+
+c=9 if a>b else 0
+print(c)
