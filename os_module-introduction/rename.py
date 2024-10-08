ef841e95e15dac61ff982fb9eda9bfbbdb2ab30c
mahruf60 <mahruf9060@gmail.com>
2024-10-09 00:52:44 +0600

    update

--- os_module-introduction/rename.py
+++ os_module-introduction/rename.py
@@ -3,7 +3,9 @@ import os
 for i in range(0, 100):
     os.rename(f"data/Day{i+1}",f"data/Tutorial{i+1}")
     
-    
+ 
+ 
+#  after rename space all file name like Tutorial 1   
 import os
 
 for i in range(0, 100):
