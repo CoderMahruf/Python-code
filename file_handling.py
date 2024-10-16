3ea2b664754807146b265d74fd80cefee2a5d347
mahruf60 <mahruf9060@gmail.com>
2024-10-16 21:12:30 +0600

    update

--- file_handling.py
+++ file_handling.py
@@ -1,2 +1,17 @@
+# File read mode 
 f = open('myfile.txt', 'r')
-print(f)
+text = f.read()
+print(text)
+
+# File Read mode 
+# f = open('myfile2.txt', 'w')
+# f.write("Hello, World!")
+# f.close()
+
+# File append mode 
+f = open('myfile2.txt', 'a')
+f.write("Hello,World!")
+f.close()
+
+with open('myfile.txt', 'a') as f:
+    f.write("Hey i am inside with")
