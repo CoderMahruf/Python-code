3d4a2906a2974bfbeb3c9220e1ced0fed236f695
mahruf60 <mahruf9060@gmail.com>
2024-10-20 21:58:09 +0600

    update

--- map_filter_reduce.py
+++ map_filter_reduce.py
@@ -8,7 +8,35 @@ l=[1,5,6,8,11,15,18]
 newl=list(map(cube,l))
 print(newl)
 
+l=[1,3,5,7,9,11]
+newl=list(map(lambda x: x*x*x,l))
+print(newl)
+
 # Double each number using the map functin 
 numbers=[1,2,3,4,5]
 doubled=list(map(lambda x: x*2,numbers))
 print(doubled)
+
+# filter 
+l=[1,3,5,7,9,11]
+def filter_function(x):
+    return x>5
+newl2=list(filter(filter_function,l))
+print(newl2)
+
+numbers=[1,2,3,4,5,7,8,9,10]
+evens=list(filter(lambda x: x % 2 == 0 ,numbers))
+print(evens)
+
+# reduce 
+from functools import reduce
+l=[1,3,5,7,9]
+def mysum(x,y):
+    return x+y
+sum=reduce(mysum,l)
+print(sum)
+
+from functools import reduce
+l=[1,3,5,7,9]
+sum=reduce(lambda x,y: x+y,l)
+print(sum)
