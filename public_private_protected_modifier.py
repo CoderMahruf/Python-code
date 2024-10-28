20dda97a1a1bd346497832cd08e7d842badbe650
mahruf60 <mahruf9060@gmail.com>
2024-10-29 00:04:52 +0600

    update

---
+++ public_private_protected_modifier.py
@@ -0,0 +1,34 @@
+# Public Access Modifier in Python 
+# by default accessible from outside 
+class Emplyee:
+    def __init__(self):
+        self.name = "Shuvo"  #public variable
+obj = Emplyee()
+print(obj.name)
+
+class Student:
+    def __init__(self,age,name):
+        self.age = age         #public variable
+        self.name = name       #public variable
+obj = Student(23,"Shuvo")
+print(obj.age)
+print(obj.name)
+
+# Private Access Modifier in python 
+class Employee:
+    def __init__(self):
+        self.__name = "Shuvo"
+obj = Employee()
+# print(obj.__dir__())   #see the all method in program using __dir__() method.
+# print(obj.__name)
+print(obj._Employee__name)
+
+# Name Mangling 
+class MyClass:
+    def __init__(self):
+        self._nonmangled_attribute = "I am a nonmangled attribute"
+        self.__mangled_attribute = "I am a mangled attribute"
+my_object = MyClass()
+print(my_object._nonmangled_attribute)
+# print(my_object.__mangled_attribute) 
+print(my_object._MyClass__mangled_attribute) 
